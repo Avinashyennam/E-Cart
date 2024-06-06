a077f0de806430f865d613564c85457ba7c3af19
Avinash <avinashyennam01@gmail.com>
2024-06-06 19:45:55 +0530

    developed the navbar but adding functionality is left

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import './Navbar.css';
-import logo from '../assets/logo.png';
+// import logo from '../assets/logo.png';
 let Navbar = ()=>{
     return(
         <>
-            <div className='flex gap-4 justify-around'>
-                <div className='flex gap-4 justify-around items-center'>
-                    <img src={logo} alt='not found' className='w-20 h-20'/>
+            <div className='flex gap-4 justify-between items-center mx-10'>
+                <div className='flex gap-1 justify-around items-center'>
+                    <img src='/logo.png' alt='not found' className='w-20 h-20'/>
                     <h1 className='logo-text text-4xl font-bold'>Cart</h1>
                 </div>
                 <div>
-                    <ul className='flex gap-4 justify-between'>
-                        <li>Shop</li>
-                        <li>Men</li>
-                        <li>Women</li>
-                        <li>Kids</li>
+                    <ul className='flex gap-8 justify-between'>
+                        <li className='text-2xl font-medium'>Shop</li>
+                        <li className='text-2xl font-medium'>Men</li>
+                        <li className='text-2xl font-medium'>Women</li>
+                        <li className='text-2xl font-medium'>Kids</li>
                     </ul>
                 </div>
-                <div className='flex gap-4 justify-between'>
-                    <div>Login</div>
-                    <div>Cart</div>
+                <div className='flex gap-4 justify-between items-center'>
+                    <div className='bg-red-500 px-2 py-1 rounded-lg'>
+                        <button className='text-xl text-white'>Login</button>
+                    </div>
+                    <div>
+                        <img src='/icons8-cart-100.png' alt='not found' width={36} height={35}/>
+                    </div>
                 </div>
             </div>
         </>
