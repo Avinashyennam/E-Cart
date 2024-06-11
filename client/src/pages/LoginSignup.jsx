13d3f0dea6fe1d0e36b1fdfe47cd203ef900f5c5
Avinash <avinashyennam01@gmail.com>
2024-06-11 08:25:08 +0530

    developed the loginsignup page but functionality is left

--- client/src/pages/LoginSignup.jsx
+++ client/src/pages/LoginSignup.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-
+import './pages.css';
 const Login = ()=>{
 
     const [userdata, setUserdata] = useState({
@@ -10,16 +10,20 @@ const Login = ()=>{
     console.log(userdata);
     return(
         <>
-            <div>
+            <div className="flex flex-col justify-center items-center p-10">
                 <div className="text-3xl">
                     <h1>Welcome to E-CART</h1>
                 </div>
-                <div>
-                    <form className="flex flex-col gap-4">
+                <div className="w-2/5">
+                    <form className="login-form flex flex-col justify-center items-center gap-4 border p-10 rounded-lg">
                         <input type="text" name="name" value={userdata.name} placeholder="Enter name" onChange={(e)=>setUserdata(e.target.value)}/>
                         <input type="email" name="email" value={userdata.email} placeholder="Enter Email" onChange={(e)=>setUserdata(e.target.value)}/>
                         <input type="password" name="password" value={userdata.password} placeholder="Enter Password" onChange={(e)=>setUserdata(e.target.value)}/>
+                        <button className="bg-white text-black text-xl w-28 p-1 rounded-lg">submit</button>
+                        <h1>Already have an account? <span className="text-blue-700">Click </span>here to login</h1>
                     </form>
+                    <div>
+                    </div>
                 </div>
             </div>
         </>
