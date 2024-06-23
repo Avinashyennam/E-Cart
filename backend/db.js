2e8fa2c7096d84ce746affdfc43ae2f1cc828c21
Avinash <avinashyennam01@gmail.com>
2024-06-23 13:15:13 +0530

    created setup for uploading images in server through multer package and uploaded one image for testing

---
+++ backend/db.js
@@ -0,0 +1,2 @@
+const mogoose = require("mongoose");
+mongoose.connect("mongodb+srv://avinashyennam02:3zeZGnl3u5bASVgR@cluster0.esoszpn.mongodb.net/Ecommerce");
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,32 @@
-const port = 4000;
+const port = 5000;
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const path = require("path");
 const multer = require("multer");
-const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
+
+// setup storage for uploaded files
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}_${path.extname(file.originalname)}`)
+    }
+});
+
+// create a multer instance
+const upload = multer({storage: storage});
+
+// creating api route to upload files
+app.use("/images", express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success: 1,
+        img_url : `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
 
 
 app.get("/",(req, res)=>{
