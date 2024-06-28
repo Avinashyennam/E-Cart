e41e7a902f8de11a4123f97ae3d3e2929f77f4ab
Avinash <avinashyennam01@gmail.com>
2024-06-28 22:35:17 +0530

    implemented basic admin design

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,6 +1,20 @@
+import './pages.css'
 const Admin = ()=>{
     return(
-        <h1>Admin</h1>
+        <div className='admin-container'>
+            <div className="box">
+                <div className='inner-box'>
+                    {/* <img src='/add-product.png' alt="not found" /> */}
+                    <h1>Add</h1>
+                </div>
+                <div className='inner-box'>
+                    <h1>Remove</h1>
+                </div>
+                <div className='inner-box'>
+                    <h1>List</h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
