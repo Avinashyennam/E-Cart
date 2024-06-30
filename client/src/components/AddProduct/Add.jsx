a167c840b652860ec292cf49237f182f47cc48f4
Avinash <avinashyennam01@gmail.com>
2024-06-30 17:18:35 +0530

    created form for adding products

--- client/src/components/AddProduct/Add.jsx
+++ client/src/components/AddProduct/Add.jsx
@@ -2,24 +2,52 @@ import './add.css';
 const AddProduct = () => {
     return (
         <div>
-            <div>
+            <div className="product-info">
                 <div>
                     <form action="">
                         <div className='col-gap'>
-                            <input type="text" placeholder="Enter name" />
-                            <input type="number" name="" id="" placeholder="Enter old price" />
+                            <div className='flex-col'>
+                                <label htmlFor="name">Enter Product name</label>
+                                <input type="text" placeholder="Enter name" />
+                            </div>
+                            <div className='flex-col'>
+                                <label>Enter old price</label>
+                                <input type="number" name="" id="" placeholder="Enter old price" />
+                            </div>
+
                         </div>
                         <div className='col-gap'>
-                            <input type="number" name="" id="" placeholder="Enter new price" />
-                            <input type="text" placeholder="Enter description" />
+                        <div className='flex-col'>
+                                <label>Enter discount</label>
+                                <input type="number" name="" id="" placeholder='Enter discount'/>
+                            </div>
+                            <div className='flex-col'>
+                                <label>Enter old price</label>
+                                <input type="number" name="" id="" placeholder="Enter old price" />
+                            </div>
+                            
                         </div>
                         <div className='col-gap'>
-                            <input type="text" placeholder="Enter category" />
-                            <input type="file" name="" id="" />
+                            <div className='flex-col'>
+                                <label>select category</label>
+                                <select name="" id="">
+                                    <option value="">kid</option>
+                                    <option value="">men</option>
+                                    <option value="">women</option>
+                                </select>
+                            </div>
+                            <div className='flex-col'>
+                                <label htmlFor="name">Enter Description</label>
+                                <input type="text" placeholder="Enter desc" />
+                            </div>
                         </div>
+                        <input type="file" name="" id="" />
                     </form>
                 </div>
-                <div>Buttons</div>
+                <div className='buttons'>
+                    <button className='add'>Add</button>
+                    <button className='remove'>Remove</button>
+                </div>
             </div>
         </div>
     )
