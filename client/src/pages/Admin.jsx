40664400cdb34f486d14e48444905579ded5573e
Avinash <avinashyennam01@gmail.com>
2024-06-30 18:45:28 +0530

    admin page implemented with images

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -5,17 +5,20 @@ const Admin = ()=>{
         <div className='admin-container'>
             <div className="box">
                 <div className='inner-box'>
-                    {/* <img src='/add-product.png' alt="not found" /> */}
+                    <img src='/add-product.png' alt="not found" />
+                    {/* <img src="/cart.png" alt="" /> */}
                     <h1>Add</h1>
                 </div>
                 <div className='inner-box'>
+                    <img src='/removeproduct.png' alt='not found' />
                     <h1>Remove</h1>
                 </div>
                 <div className='inner-box'>
+                    <img src='/listofproducts.png' alt='not found' />
                     <h1>List</h1>
                 </div>
             </div>
-            <AddProduct />
+            {/* <AddProduct /> */}
         </div>
     )
 }
