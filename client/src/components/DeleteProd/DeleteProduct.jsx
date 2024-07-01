309a5c597b68a039ed6af5c6acbe4f91e7929d4e
Avinash <avinashyennam01@gmail.com>
2024-07-01 14:28:23 +0530

    implemented conditional rendering of add, remove and listProduct components

--- client/src/components/DeleteProd/DeleteProduct.jsx
+++ client/src/components/DeleteProd/DeleteProduct.jsx
@@ -1,5 +1,21 @@
 import './delete.css'
+import { useEffect, useState } from 'react';
 const DeleteProduct = ()=>{
+
+    const [delData, setDelData] = useState({
+        id:'',
+        name:''
+    })
+    const handleDelete = (e)=>{
+        const {name, value} = e.target;
+        setDelData({
+            ...delData,
+            [name]:value
+        })
+    }
+    useEffect(()=>{
+        console.log(delData);
+    },[delData]);
     return(
         <div className="delete">
             <div className='heading'>Delete Product</div>
@@ -7,11 +23,11 @@ const DeleteProduct = ()=>{
                 <form action="">
                     <div className='flex-col'>
                         <label>Enter product id</label>
-                        <input type="number" name="" id="" />
+                        <input type="number" name="id" value={delData.id} id="" onChange={handleDelete}/>
                     </div>
                     <div className='flex-col'>
                         <label>Enter product name</label>
-                        <input type="text" name="" id="" />
+                        <input type="text" name="name" value={delData.name} id="" onChange={handleDelete}/>
                     </div>
                     <button>Delete</button>                    
                 </form>
--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -3,28 +3,51 @@ import { useState } from 'react';
 import AddProduct from '../components/AddProduct/Add';
 import ListProducts from '../components/ListProducts/ListProducts';
 import DeleteProduct from '../components/DeleteProd/DeleteProduct';
-const Admin = ()=>{
+const Admin = () => {
 
     const [view, setView] = useState("");
-    return(
+    const handleAdd = () => {
+        setView("add");
+    }
+    const handleDelete = () => {
+        setView("delete");
+    }
+    const handleList = () => {
+        setView("list");
+    }
+    return (
         <div className='admin-container'>
             <div className="box">
-                <div className='inner-box'>
-                    <img src='/add-product.png' alt="not found" width="100"/>
-                    <h1>Add</h1>
-                </div>
-                <div className='inner-box'>
-                    <img src='/removeproduct.png' alt='not found' width="100"/>
-                    <h1>Remove</h1>
-                </div>
-                <div className='inner-box'>
-                    <img src='/listofproducts.png' alt='not found' width="100"/>
-                    <h1>List</h1>
-                </div>
+                <button onClick={handleAdd} className={`${view === "add" ? 'border':''}`}>
+                    <div className='inner-box'>
+                        <img src='/add-product.png' alt="not found" width="100" />
+                        <h1>Add</h1>
+                    </div>
+                </button>
+                <button onClick={handleDelete} className={`${view === "delete" ? 'border':''}`}>
+                    <div className='inner-box'>
+                        <img src='/removeproduct.png' alt='not found' width="100" />
+                        <h1>Remove</h1>
+                    </div>
+                </button>
+                <button onClick={handleList} className={`${view === "list" ? 'border':''}`}>
+                    <div className='inner-box'>
+                        <img src='/listofproducts.png' alt='not found' width="100" />
+                        <h1>List</h1>
+                    </div>
+                </button>
             </div>
-            <AddProduct />
-            {/* <ListProducts /> */}
-            {/* <DeleteProduct /> */}
+            {
+                view === "add" && <AddProduct />
+            }
+            {
+                view === "delete" && <DeleteProduct />
+            }
+            {
+                view === "list" && <ListProducts />
+            }
+
+
         </div>
     )
 }
